62323476d5437ea6903812946695e28190bd57b6
Hudiak-Mykola <mykola.hudiak@gmail.com>
2022-07-21 23:42:59 +0300

    JS hw1

---
+++ lesson1/main.js
@@ -0,0 +1,71 @@
+let n1 = 'hello'
+console.log(n1)
+alert(n1)
+document.write(n1)
+let n2 = 'owu'
+console.log(n2)
+alert(n2)
+document.write(n2)
+let n3 = 'com'
+console.log(n3)
+alert(n3)
+document.write(n3)
+let n4 = 'ua'
+console.log(n4)
+alert(n4)
+document.write(n4)
+let n5 = 1
+console.log(n5)
+alert(n5)
+document.write(n5)
+let n6 = 10
+console.log(n6)
+alert(n6)
+document.write(n6)
+let n7 = -999
+console.log(n7)
+alert(n7)
+document.write(n7)
+let n8 = 3.14
+console.log(n8)
+alert(n8)
+document.write(n8)
+let n9 = 2.7
+console.log(n9)
+alert(n9)
+document.write(n9)
+let n10 = 16
+console.log(n10)
+alert(n10)
+document.write(n10)
+let n11 = true
+console.log(n11)
+alert(n11)
+document.write(n11)
+let n12 = false
+console.log(n12)
+alert(n12)
+document.write(n12)
+
+
+let firstName = 'Микола '
+let middleName = 'Федорович '
+let lastName = 'Гудяк'
+let person = (firstName+middleName+lastName)
+console.log(person)
+document.write(person)
+
+
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof b)
+let c = true;
+console.log(typeof c)
+
+let name = prompt('Введіть ваше імя')
+console.log(name)
+let name2 = prompt('По-батькові')
+console.log(name2)
+let birth = prompt('Дата народження')
+console.log(birth)
